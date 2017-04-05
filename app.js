0604856dc74ae23af43e615846dcc6ca68863ba4
RyleeAndrews <ryleeandrews73@gmail.com>
2017-04-05 14:25:01 -0700

    making changes

--- app.js
+++ app.js
@@ -5,49 +5,57 @@ console.log('The Guessing Game');
 
 var firstCode = prompt('Did I enjoy my first coding session?');
 console.log('Did I do it well though?', firstCode);
-if (firstCode.toLowerCase() == 'yes'){
-  alert ('Yup loved it!');
-
-
+if(firstCode == null){
+  alert('please enter something');
 }
-else {
+else if(firstCode.toLowerCase() == 'yes'){
+  alert('Yup loved it!');
+}
+else{
   alert('Nope fell in love right away.');
-
 }
 var bucky = prompt('Did I first learn online with Bucky?');
 console.log('Was Bucky the instructor?', bucky);
-if (bucky.toUpperCase() == 'YES') {
-  alert ('Great teacher!');
+if(bucky == null){
+  alert('please enter something');
 }
-else {
-  alert ('You\'re wrong where else would I go to learn the basics.');
-}
-
-var sam = prompt('Did Sam convince me to get into coding?');
-console.log('Did he fully convince me though?', sam);
-
-if (sam.toLowerCase() == 'yes'){
-  alert ('Thank god for that.');
+ else if(bucky.toUpperCase() == 'YES') {
+  alert('Great teacher!');
 }
 else{
-  alert ('Wrong, he did and am so happy he did.');
+  alert('You\'re wrong where else would I go to learn the basics.');
 }
-
+var sam = prompt('Did Sam convince me to get into coding?');
+console.log('Did he fully convince me though?', sam);
+if(sam == null){
+  alert('please enter something');
+}
+else if(sam.toLowerCase() == 'yes'){
+  alert('Thank god for that.');
+}
+else{
+  alert('Wrong, he did and am so happy he did.');
+}
 var favoriteLanguage = prompt('Is css my favorite language?');
 console.log('Or is it javascript?', favoriteLanguage);
-
-if (favoriteLanguage.toUpperCase() == 'NO'){
+if(favoriteLanguage == null){
+  alert('please enter something');
+}
+else if(favoriteLanguage.toUpperCase() == 'NO'){
   alert('Right but it\'s still fun');
 }
-else {
-  alert ('No javascript is.');
+else{
+  alert('No javascript is.');
 }
 var favoriteSushi = prompt ('Is the spider roll my favorite roll of sushi?');
 console.log('Or is it spicy tuna?', favoriteSushi);
-
-if (favoriteSushi.toLowerCase() == 'yes'){
-  alert ('It\'s so bomb!');
+if(favoriteSushi == null){
+  alert('please enter something');
 }
-else {
-  alert ('No but spicy tuna is also one of my go to\'s.');
+else if(favoriteSushi.toLowerCase() == 'yes'){
+  alert('It\'s so bomb!');
 }
+else{
+  alert('No but spicy tuna is also one of my go to\'s.');
+}
+ 
